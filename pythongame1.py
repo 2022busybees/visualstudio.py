105866a63e4ef57f03e3b47911315cf362b838b2
Sidra Umair <112921299+2022busybees@users.noreply.github.com>
2023-11-12 02:33:20 +0500

    Add files via upload

---
+++ pythongame1.py
@@ -0,0 +1,34 @@
+import pygame
+
+pygame.init()
+
+width = 800
+height = 800
+white_colour = (255, 255, 255)
+
+game_window = pygame.display.set_mode((width,height))
+
+clock = pygame.time.Clock()
+
+
+def run_game_loop():
+    while True:
+        # Handle events
+        events = pygame.event.get()
+        for event in events:
+            if event.type == pygame.QUIT:
+             return
+        
+        # Execute logic
+        # Update display
+        game_window.fill(white_colour)
+        pygame.display.update()
+
+
+    clock.tick(60)
+
+
+run_game_loop()
+
+pygame.quit()
+quit()
